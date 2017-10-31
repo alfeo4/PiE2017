c1dd10b58661ac84bf3c6169439ff40f9f801a7d
Reinout Nonhebel <reinoutn@hotmail.com>
2017-10-31 11:28:50 +0100

    Added matrix-matrix multiplication.

--- Raytracer PiE Eindopdracht/Matrix22.h
+++ Raytracer PiE Eindopdracht/Matrix22.h
@@ -9,13 +9,13 @@ template <typename T>
 class Matrix22
 {
 public:
-    T& operator[] (int index);
+    T& operator[] (int index); // Overloaded subscript operator
 
 protected:
     T m_data[4];
 };
 
-// Access vector elements
+// Return a reference to a selected element (by index)
 template <typename T>
 T& Matrix22<T>::operator[] (int index)
 {
@@ -24,6 +24,21 @@ T& Matrix22<T>::operator[] (int index)
         // TODO throw error?
     }
     return m_data[index];
+}
+
+// Multiply two 2-by-2 matrices
+template <typename T>
+Matrix22<T> operator* (Matrix22<T> A, Matrix22<T> B)
+{
+    Matrix22<T> C;
+    // First row
+    C[0] = A[0]*B[0] + A[1]*B[2];
+    C[1] = A[0]*B[1] + A[1]*B[3];
+    // Second row
+    C[2] = A[2]*B[0] + A[3]*B[2];
+    C[3] = A[2]*B[1] + A[3]*B[3];
+
+    return C;
 }
 
 #endif // MATRIX22_H
