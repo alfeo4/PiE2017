de4dbd5c5d1256c14935a1b168c6ffd3bc9f3ce9
Reinout Nonhebel <reinoutn@hotmail.com>
2017-10-31 12:56:07 +0100

    Added Component base class.
    Actual component types (Medium, interfaces, lenses, etc) can inherit from
    this base class.

---
+++ Raytracer PiE Eindopdracht/Component.cpp
@@ -0,0 +1,14 @@
+#include "Component.h"
+
+void Component::setMatrix(double A, double B, double C, double D)
+{
+    m_matrix[0] = A;
+    m_matrix[1] = B;
+    m_matrix[2] = C;
+    m_matrix[3] = D;
+}
+
+Matrix22 Component::getMatrix()
+{
+    return m_matrix;
+}
