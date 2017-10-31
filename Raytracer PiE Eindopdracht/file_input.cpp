92abaa6cae72b27b7d11e1baf394d38ee09369b6
Paul <alfeo4@gmail.com>
2017-10-31 12:29:30 +0100

    Cleaned commit, project files match and file_input has been functionalised

--- Raytracer PiE Eindopdracht/file_input.cpp
+++ Raytracer PiE Eindopdracht/file_input.cpp
@@ -7,18 +7,13 @@
 
 
 
-main()
+std::vector<std::string> openFile(std::string nameOfFile)
 {
 
     using namespace std;
-    //Ask user which file to read
-    string filepath;
-/*
-    cout << "please type full path to file: " << endl;
-    cin >> filepath;
-*/
-    filepath = "C:/Users/User/Documents/PiE/Eindopdracht/PiE2017/Raytracer PiE Eindopdracht/Test_for_file_io.txt";
-    const char * file = filepath.c_str(); // ifstream doesn't like strings, so change to a const char*
+
+    //filepath = "C:/Users/User/Documents/PiE/Eindopdracht/PiE2017/Raytracer PiE Eindopdracht/Test_for_file_io.txt";
+    const char * file = nameOfFile.c_str(); // ifstream doesn't like strings, so change to a const char*
 
     //Create an input stream to read the file line by line and put the resulting info into string array input, with one line per index.
     string line;
@@ -38,18 +33,14 @@ main()
 
     }
     ins.close();
-
-
-    for (int i = 0; i < input.size(); i++)
-    {
-        cout << input[i] << endl;
-    }
-
-
-
-
-
+    return input;
 
 
 }
+
+
+
+
+
+
 
