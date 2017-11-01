276b90f5bbd896cbc17d52f6291069ac957482fe
Paul <alfeo4@gmail.com>
2017-11-01 10:16:12 +0100

    Added fileInput and parsing

---
+++ Raytracer PiE Eindopdracht/fileInput.h
@@ -0,0 +1,8 @@
+
+#ifndef FILEINPUT.H
+#define FILEINPUT.H
+
+std::vector<std::string> openFile(std::string nameOfFile);
+
+
+#endif // FILEINPUT
---
+++ Raytracer PiE Eindopdracht/parseInput.h
@@ -0,0 +1,11 @@
+#ifndef PARSEINPUT.H
+#define PARSEINPUT.H
+
+#include <vector>
+#include "DataContainer.h"
+
+
+std::vector<DataContainer> parseInput(std::vector<string> input);
+
+
+#endif // PARSEINPUT
