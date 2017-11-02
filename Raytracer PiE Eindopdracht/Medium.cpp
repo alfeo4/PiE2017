6d38884bffae233bc01390a62cb7de0342fd7c8d
Reinout Nonhebel <reinoutn@hotmail.com>
2017-11-02 11:37:15 +0100

    Removed refractive index property from Medium class. (Issue #1)

--- Raytracer PiE Eindopdracht/Medium.cpp
+++ Raytracer PiE Eindopdracht/Medium.cpp
@@ -3,14 +3,6 @@
 Medium::Medium(double length)
 {
     m_length = length;
-    m_index = 1;
-    generateMatrix();
-}
-
-Medium::Medium(double length, double index)
-{
-    m_length = length;
-    m_index = index;
     generateMatrix();
 }
 
--- Raytracer PiE Eindopdracht/Medium.h
+++ Raytracer PiE Eindopdracht/Medium.h
@@ -3,21 +3,18 @@
 
 #include "Component.h"
 
-/* A component class that represents a transparent medium,
-*  with a length and a refractive index.
+/* A component class that represents a transparent medium
+*  with a given length. Refractive index is not relevant for
+*  propagation, only for refraction at surfaces.
 */
-
 
 class Medium : public Component
 {
     public:
-        // Two constructors, so the refractive index defaults to 1 when omitted.
         Medium(double length);
-        Medium(double length, double index);
 
     private:
         double m_length;
-        double m_index;
 
         // Generate the transfer matrix from the basic properties.
         void generateMatrix();
