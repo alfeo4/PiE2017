ddf47d71690bbbe55ebc4b4a089efc2c038ad57d
Reinout Nonhebel <reinoutn@hotmail.com>
2017-11-02 15:59:13 +0100

    Added writeRays function
    A vector of RayContainer objects can now be written to a file.

---
+++ Raytracer PiE Eindopdracht/writeRays.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iostream>
+
+#include "RayContainer.h"
+
+/* Write a RayContainer vector to a file. Each ray on a new line with distance and inclination
+*  separated by a space.
+*/
+
+void writeRays(const std::vector<RayContainer>& rays, const std::string fileName)
+{
+    std::ofstream outFile(fileName.c_str(), std::ios::out);
+
+    if(!outFile.is_open())
+    {
+        std::cerr << "Could not open file: " << fileName << std::endl;
+        return;
+    }
+
+    for(RayContainer ray : rays)
+    {
+        outFile << ray.getDistance() << " " << ray.getInclination() << '\n';
+    }
+
+    outFile.close();
+
+}
