f4bb0e92f082ef134486e4707018cbaa7c14a862
Paul <alfeo4@gmail.com>
2017-11-03 12:33:58 +0100

    Changes to main for testing.

--- Raytracer PiE Eindopdracht/main.cpp
+++ Raytracer PiE Eindopdracht/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <vector>
+#include "SystemContainer.h"
 
 using namespace std;
-/*
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+
+    filepath = "Test_for_file_io.txt";
+    vector<string> test0 = openFile(filepath);
+    vector<SystemContainer> test1 = parseSystem(test0);
+    OpticalSystem test2 =
+
+
+
+    filepath2 = "Test_for_ray_io.txt";
+    vector<string> test99 = openFile(filePath2);
+    vector<RayContainer> test98 = parseRay(test99);
+
+    vector<RayContainer> output = calculateOutput(test98, test2);
+    cout << output[0].getDistance() << endl;
+
+
+
 }
-*/
+
