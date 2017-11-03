775194a3cd10f79e815f176c9f4772d81934876f
Reinout Nonhebel <reinoutn@hotmail.com>
2017-11-03 15:14:25 +0100

    Added expandLenses to main project

---
+++ Raytracer PiE Eindopdracht/expandLenses.h
@@ -0,0 +1,10 @@
+#ifndef EXPANDLENSES_H
+#define EXPANDLENSES_H
+
+#include <string>
+
+using namespace std;
+
+void expandLenses(string lensfile, string componentfile);
+
+#endif // EXPANDLENSES_H
--- systembuilder/fileInput.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef FILEINPUT_H
-#define FILEINPUT_H
-
-std::vector<std::string> openFile(std::string nameOfFile);
-
-
-#endif // FILEINPUT
