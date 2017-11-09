ef8294e49350e920523d91e8a3cf20181a43c1fe
Reinout Nonhebel <reinoutn@hotmail.com>
2017-11-09 12:17:23 +0100

    Draw not only rays, but also interfaces from matlab

--- Raytracer PiE Eindopdracht/drawsystem.m
+++ Raytracer PiE Eindopdracht/drawsystem.m
@@ -20,3 +20,13 @@ xPositions = cumsum([0 thickness']);
 % Load ray data
 raydata = importdata('outrays.txt', ' ', 0); % delimiter space, 0 header lines
 plot(xPositions, raydata)
+
+% Find the maximum distance from the optical axis for *any* ray.
+systemWidth = 1.1*max(max(abs(raydata))); 
+
+% plot lines at interfaces
+for (x = xPositions)
+    line([x x], [-systemWidth systemWidth],'Color',[0.2 0.2 0.2]);
+end
+
+axis([-0.1*xPositions(end), 1.1*xPositions(end), -1.2*systemWidth, 1.2*systemWidth])
