f8934686fa8668a562d35dc5d601316e5dbc9b4a
Paul <alfeo4@gmail.com>
2017-11-09 13:35:39 +0100

    Added comments to trace.cpp and writedistances.cpp

--- Raytracer PiE Eindopdracht/Trace.cpp
+++ Raytracer PiE Eindopdracht/Trace.cpp
@@ -3,27 +3,32 @@
 #include "RayContainer.h"
 #include "Trace.h"
 
-
+// Trace will take as arguments a vector of raycontainers and an optical system.
+// The function then calculates for each ray the position of the ray (from the z-axis of the system) at a component.
+// These distances are placed in a vector of doubles for each ray. Each vector belonging to a ray is then placed in another vector.
+// This vector of vectors is returned by the function.
 
 std::vector<std::vector<double>> trace(std::vector<RayContainer> &inputRays, OpticalSystem &system)
 {
-
+    // Declare and resize a return vector
     std::vector<std::vector<double>> distances;
     distances.resize(inputRays.size());
-
+    // Get all components of the system
     std::vector<Component*> components = system.getComponents();
 
+    // Loop over all rays. Make a vector named path which holds the distance values for this ray.
     for (unsigned int i = 0; i < inputRays.size(); i++)
     {
         vector<double> path;
         Vector2<double> traceRay = inputRays[i].getRay();
-
+        // First, store the starting height of the ray
         path.push_back(traceRay[0]);
-
+        // Loop over components. For each component, figure out how the ray is transformed and store this info.
+        // Then, place the distance info in the path vector.
         for (unsigned int j = 0; j < components.size(); j++)
         {
             traceRay *= components[j]->getMatrix();
-
+            // We only need half the distances as the others are duplicates, which is taken care of by this if-statement. Also, we aren't interested in the inclination, so we ignore it.
             if (j % 2 == 0)
             {
                 path.push_back(traceRay[0]);
@@ -31,6 +36,7 @@ std::vector<std::vector<double>> trace(std::vector<RayContainer> &inputRays, Opt
             }
 
         }
+        // Add all distances for the ray to the distances vector.
         distances.push_back(path);
     }
 
--- Raytracer PiE Eindopdracht/WriteDistances.cpp
+++ Raytracer PiE Eindopdracht/WriteDistances.cpp
@@ -1,15 +1,18 @@
 #include "WriteDistances.h"
 
+// A function which writes the distances, as calculated by the trace function, to a file.
+
 void writeDistances(const std::vector<std::vector<double>>& distances, const std::string fileName)
 {
+    // open a file writing stream
     std::ofstream outFile(fileName.c_str(), std::ios::out);
-
+    // Check if opening was successful
     if(!outFile.is_open())
     {
         std::cerr << "Could not open file: " << fileName << std::endl;
         return;
     }
-
+    // Write all distances to a file, so that each line contains all distances of each ray
     for (unsigned int i = 0; i < distances.size(); i++)
     {
         for (unsigned int j = 0; j < distances[i].size(); j++)
